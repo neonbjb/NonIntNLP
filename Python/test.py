e8e820ead8fe24c3930b76eac6ce09de692ab06d
James Betker <jbetker@gmail.com>
2020-03-14 15:48:31 -0600

    Fix up the tester
    - Allow it to generate from a CSV file where we got our data from
    - Fix missing BOS token. This had a huge impact on the performance of the model.

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,6 @@
 import transformers
 import torch
-import os
-
+import math
 
 model_name = "xlnet-base-cased"
 
@@ -16,64 +15,8 @@ config.mem_len = 1024
 model = transformers.XLNetLMHeadModel.from_pretrained(output_dir, config=config)
 model.eval()
 
-# Test the model.
-actual_article_title = "Italy announces lockdown as global coronavirus cases surpass 105,000"
-article_text1 = """
-Italian Prime Minister Giuseppe Conte signed a decree early Sunday that will put millions of people across northern Italy under lockdown due to the novel coronavirus.
-The sweeping move puts the entire Lombardy region, as well as 14 other provinces, under travel restrictions, and is one of the toughest responses implemented outside of mainland China to get the Covid-19 epidemic under control.
-CNN is verifying exactly when the lockdown will go into effect.
-The announcement came after Italy saw a dramatic spike of 1,247 confirmed novel coronavirus cases on Saturday, the Civil Protection Department said in a statement.
-The country has now recorded 5,883 cases and 233 deaths, the most fatalities outside mainland China and the biggest outbreak in Europe.
-Announcing the new measures, Conte said: "There will be an obligation to avoid any movement of people who are either entering or leaving" the affected areas. "Even within the areas moving around will occur only for essential work or health reasons," he said, according to Reuters.
-While the lockdown only applies to northern Italy, other measures will be applied to the entire country. These include the suspension of schools, university classes, theaters and cinemas, as well as bars, nightclubs, and sports events. Religious ceremonies, including funerals, will also be suspended.
-Other countries in Europe are also struggling to contain outbreaks as cases continue to rise.
-On Saturday, France's general director of health, Jerome Salomon, confirmed 16 dead and 949 infected nationwide, and Germany now has 795 cases. The United Kingdom confirmed a second death from the novel coronavirus on Saturday, while 206 people have tested positive, British health officials said in a statement.
-The World Health Organization (WHO) has called on "all countries to continue efforts that have been effective in limiting the number of cases and slowing the spread of the virus."
-In a statement, the WHO said: "Allowing uncontrolled spread should not be a choice of any government, as it will harm not only the citizens of that country but affect other countries as well."
-Meanwhile in China, search and rescue efforts continued on Sunday for survivors from the collapse of a hotel that was being used as a coronavirus quarantine center.
-The hotel, in the southeastern city of Quanzhou, in Fujian province, came down Saturday night with 80 people inside. Four people died, one person remains in critical condition and four others are seriously injured, according to China's Ministry of Emergency Management.
-"We are using life detection instruments to monitor signs of life and professional breaking-in tools to make forcible entries. We are trying our utmost to save trapped people," said Guo Yutuan, squadron leader of the Quanzhou armed police detachment's mobile unit.
-The building's owner is in police custody, according to state news agency Xinhua and an investigation is underway.
-"""
-
-article_text2 = """
-Safety concerns accompanied by strong winds and power outages in ski country forced Eldora Mountain to stay closed on Thursday, March 12. As of 7:56 AM, the resort announced the closure of all lifts due to “power outages, high winds, and safety concerns.”
-Wind gusts are forecast to reach up to 20 mph in Nederland, with winds of up to 11 mph expected for the Boulder area.Elsewhere in ski country, specifically at Aspen, concerns have been raised about a possible COVID-19 outbreak. Polis has urged those older than 60 to avoid high country travel due to the virus. 
-There has been no indication that COVID-19 had anything to do with the “safety concerns” that shut down Eldora.
-Safety concerns that shut down Eldora are likely wind-related. While wind-related lift closures are common for resorts in the high country, full closures are typically rare. It’s reasonable to believe that the resort will reopen tomorrow if winds subside, but find official updates here.
-"""
-
-article_text3 = """
-A little under a year and a half after it released its first pair of true wireless earbuds, Sennheiser is back with a follow-up: the Sennheiser Momentum True Wireless 2. The big improvements to the true wireless earbuds are that they support noise cancellation and have much better battery life. There are also some more minor improvements, like the fact that these earbuds are 2mm smaller than their predecessors.
-
-The improvements in battery life are, on paper, at least, pretty impressive. You’ll now get up to seven hours of playback from the earbuds themselves (up from four hours last time around), and using the case gets you 28 hours in total (up from 12). Sennheiser also claims to have fixed the battery drain problems that some users reported with the first-generation earbuds. 
-It says it’s switch to a new Bluetooth chip that “counteracts any power drain possibilities.
-
-Rounding out the specs, the Sennheiser Momentum True Wireless 2 come with support for the Bluetooth 5.1, AAC, and AptX standards, an IPX4 water resistance rating, and have an audio passthrough mode alongside their noise-canceling mode. You can listen to just one earbud at a time, but the functionality only works with the right earbud, unfortunately. They’ll retail for $299.99 (£279.99 / €299.99), the same as their predecessors.
-
-We were impressed with the first-generation Sennheiser Momentum True Wireless when we reviewed them back in 2018, noting at the time that they were the best-sounding true wireless earbuds we’d ever heard. However, functionally, they left a little to be desired, with flaky wireless connectivity and controls that were a little unintuitive at first. And those battery drain problems were never fully addressed. We’ll be checking out whether their successors have been able to overcome these problems in our full review, which will be on the way soon.
-
-The Sennheiser Momentum True Wireless 2 will be available in the US and Europe in black starting in April, with a white variant following later.”
-"""
-
-article_text4 = """
-Trump is already under fire for visiting his properties in both countries as president, leading to U.S. taxpayer money being spent at his own firms.
- The president has been saddled with lawsuits and investigations throughout his term alleging that he’s violating the Constitution’s emoluments clause 
- by accepting taxpayer money other than his salary. The U.S. government proclamation initiating the ban targets 26 European countries that comprise a 
- visa-free travel zone known as the Schengen Area. The United Kingdom, which is home to Trump Turnberry and Trump International Golf Links, and Ireland, 
- which is home to another Trump-branded hotel and golf course at Doonbeg, don't participate in the Schengen Area. Bulgaria, Croatia and Romania are also
-  not part of the Schengen Area. All three of the resorts are struggling financially. Ireland’s prime minister, Leo Varadkar, is scheduled to meet Trump 
-  at the White House on Thursday in one of the few events related to St. Patrick’s Day that has not been canceled due to coronavirus concerns. 
-  The administration’s European travel proclamation notes that “the Schengen Area has exported 201 COVID-19 cases to 53 countries. Moreover, the
-   free flow of people between the Schengen Area countries makes the task of managing the spread of the virus difficult.” Trump’s European travel ban comes 
-   with several other loopholes. Though they are subject to border checks on arrival, residents of the 26 Schengen Area countries are also free to live and 
-   work in the United Kingdom, meaning they could fly to the United States from a British airport as long as they hadn't spent time within the Schengen 
-   countries in the last 14 days. EU leaders condemned Trump's move on Thursday, and disputed the president's criticism of Europe's handling of the crisis. 
-   “The Coronavirus is a global crisis, not limited to any continent and it requires cooperation rather than unilateral action,” European Commission President 
-   Ursula von der Leyen and European Council President Charles Michel said in a joint statement.
-"""
-import math
-
+# Load some test data
+test_data_file = "C:/Users/jbetk/Documents/data/ml/title_prediction/all-the-news/articles1.csv"
 
 # Create inputs to the model for a given chunk of input_ids and a partially generated output.
 def create_inputs_for_chunk(_chunk: torch.Tensor,
@@ -109,7 +52,7 @@ def predict_words(_text_tensor: torch.Tensor,
 
     _tok_text_chunked = torch.chunk(_text_tensor, _chunk_count, dim=0)
     _mems = None
-    for _chunk in _tok_text_chunked:
+    for _chunk in reversed(_tok_text_chunked):
         _inputs = create_inputs_for_chunk(_chunk, _outputs_so_far, _mems, _tokenizer, _test_device)
         _logits, _mems = _test_model.forward(**_inputs)
 
@@ -154,9 +97,38 @@ def test_model(_text_input: str,
             _tokenizer.encode(_text_input + _tokenizer.eos_token, add_special_tokens=False),
             dtype=torch.long)
         _tok_title = torch.full((_title_len,), _tokenizer.mask_token_id, dtype=torch.long)
-        _predicted_tensor = predict_forward(_tok_text, _tok_title, 0, _tokenizer, _test_model, _seq_len, _title_len,
+        _tok_title[0] = tokenizer.bos_token_id
+        _predicted_tensor = predict_forward(_tok_text, _tok_title, 1, _tokenizer, _test_model, _seq_len, _title_len,
                                             _test_device)
         return _tokenizer.decode(_predicted_tensor)
 
+def process_csv_line(line):
+    TITLE_INDEX = 2
+    CONTENT_INDEX = 9
+    splitted = line.split(",")
+
+    # Once the "content" line begins, any number of commas can appear before the newline which must not be parsed.
+    rejoined_content = ",".join(splitted[CONTENT_INDEX:-1])
+
+    # Don't accept content with too small of text content or title content. Often these are very bad examples.
+    if len(rejoined_content) < 1024:
+        return None
+    if len(splitted[TITLE_INDEX]) < 30:
+        return None
+
+    return {"title": splitted[TITLE_INDEX], "content": rejoined_content}
+
+#for i in range(10):
+#    print(test_model(article_text4, tokenizer, model, CHUNK_SEQ_LEN, TITLE_PRED_MAX_LEN, torch.device("cuda")))
+
 model.to("cuda")
-print(test_model(article_text4, tokenizer, model, CHUNK_SEQ_LEN, TITLE_PRED_MAX_LEN, torch.device("cuda")))
+with open(test_data_file, encoding="utf-8") as file:
+    line = file.readline()
+    while line:
+        processed = process_csv_line(line)
+        if processed is not None:
+            print("Input text: " + processed["content"])
+            print(test_model(processed['content'], tokenizer, model, CHUNK_SEQ_LEN, TITLE_PRED_MAX_LEN, torch.device("cuda")))
+            print("Actual title: " + processed['title'])
+            print("\n\n")
+        line = file.readline()
