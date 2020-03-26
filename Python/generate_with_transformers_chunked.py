01373492195b3c212163432d57856ee01581878a
James Betker <jbetker@gmail.com>
2020-03-26 16:38:04 -0600

    Black generator

--- Python/generate_with_transformers_chunked.py
+++ Python/generate_with_transformers_chunked.py
@@ -28,16 +28,10 @@ parser.add_argument(
     help="The number of test_data rows to pull and generate from.",
 )
 parser.add_argument(
-    "--device",
-    type=str,
-    default="cuda",
-    help="The device to use for generation.",
+    "--device", type=str, default="cuda", help="The device to use for generation.",
 )
 parser.add_argument(
-    "--do_sampling",
-    type=bool,
-    default=False,
-    help="The device to use for generation.",
+    "--do_sampling", type=bool, default=False, help="The device to use for generation.",
 )
 args = parser.parse_args()
 
@@ -74,23 +68,26 @@ test_set = ChunkedTextDataset(
 random.seed(12345)
 device = torch.device(device_str)
 model.to(device)
-results = { "meta": chunk_config,
-            "results": []}
+results = {"meta": chunk_config, "results": []}
 for i in range(number_to_generate):
-    chunked_data = test_set[random.randint(0, len(test_set)-1)]
+    chunked_data = test_set[random.randint(0, len(test_set) - 1)]
     full_text = []
 
     # Start by establishing mems state - achieved by doing forward passes on all chunks but the last one.
     mems = None
     num_chunks = len(chunked_data["input_ids"])
-    for c in range(num_chunks-1):
+    for c in range(num_chunks - 1):
         full_text.extend(chunked_data["input_ids"][c].tolist())
 
         model_inputs = {
             "input_ids": chunked_data["input_ids"][c].unsqueeze(0).to(device),
-            "attention_mask": chunked_data["attention_masks"][c].unsqueeze(0).to(device),
+            "attention_mask": chunked_data["attention_masks"][c]
+            .unsqueeze(0)
+            .to(device),
             "perm_mask": chunked_data["permutation_masks"][c].unsqueeze(0).to(device),
-            "target_mapping": chunked_data["target_mappings"][c].unsqueeze(0).to(device),
+            "target_mapping": chunked_data["target_mappings"][c]
+            .unsqueeze(0)
+            .to(device),
         }
         if mems is not None:
             model_inputs["mems"] = mems
@@ -106,30 +103,38 @@ for i in range(number_to_generate):
 
     # Use the transformers generate function to do the actual generation now.
     if do_sampling:
-        genned_results = model.generate(prompt_inputs, max_length=chunk_seq_len,
-                       do_sample=True,
-                       num_beams=7,
-                       temperature=.7,
-                       top_k=0,
-                       top_p=.9,
-                       repetition_penalty=5,
-                       eos_token_ids=tokenizer.eos_token_id,
-                       num_return_sequences=5,
-                       mems=mems)
+        genned_results = model.generate(
+            prompt_inputs,
+            max_length=chunk_seq_len,
+            do_sample=True,
+            num_beams=7,
+            temperature=0.7,
+            top_k=0,
+            top_p=0.9,
+            repetition_penalty=5,
+            eos_token_ids=tokenizer.eos_token_id,
+            num_return_sequences=5,
+            mems=mems,
+        )
     else:
-        genned_results = model.generate(prompt_inputs, max_length=chunk_seq_len,
-                       do_sample=False,
-                       num_beams=12,
-                       repetition_penalty=5,
-                       eos_token_ids=tokenizer.eos_token_id,
-                       num_return_sequences=2,
-                       mems=mems)
+        genned_results = model.generate(
+            prompt_inputs,
+            max_length=chunk_seq_len,
+            do_sample=False,
+            num_beams=12,
+            repetition_penalty=5,
+            eos_token_ids=tokenizer.eos_token_id,
+            num_return_sequences=2,
+            mems=mems,
+        )
 
     # Append results here.
     seqs, _ = genned_results.shape
     genned_texts = []
     prompt = tokenizer.decode(prompt_inputs[0])
-    print("\n------------------------------------------------------------------------------")
+    print(
+        "\n------------------------------------------------------------------------------"
+    )
     print("PROMPT: `%s`" % (prompt))
     for s in range(seqs):
         genned_texts.append(tokenizer.decode(genned_results[s][text_len:]))
@@ -137,9 +142,11 @@ for i in range(number_to_generate):
     result = {
         "prompt": tokenizer.decode(full_text),
         "generated": genned_texts,
-        "actual": tokenizer.decode(chunked_data["labels"])
+        "actual": tokenizer.decode(chunked_data["labels"]),
     }
-    print("------------------------------------------------------------------------------")
+    print(
+        "------------------------------------------------------------------------------"
+    )
     results["results"].append(result)
 
 # Record the results.
